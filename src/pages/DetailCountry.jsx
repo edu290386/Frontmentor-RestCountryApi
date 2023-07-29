76fb54958696f4894e0e1289c44bfccf382a3a45
edu290386 <eduardo.torres@outlook.com>
2023-07-29 13:03:28 -0500

    country responsive

--- src/pages/DetailCountry.jsx
+++ src/pages/DetailCountry.jsx
@@ -6,7 +6,7 @@ const DetailCountry = () => {
   const { nameCountry } = useParams();
 
   const detailCountry = data.filter((country) => country.name == nameCountry);
-
+    console.log(detailCountry)
   return (
     <div className="mx-6">
       <section className="bg-very-light-gray  pt-6 h-screen mx-auto">
@@ -16,14 +16,36 @@ const DetailCountry = () => {
             Back
           </button>
         </div>
-        <article className="my-16 mx-auto flex flex-col  sm:flex-row sm:justify-between">
+        <article className="my-12 mx-auto flex flex-col  sm:flex-row sm:justify-between">
           <img
             className="w-full max-w-[330px] h-[240px]"
             src={detailCountry[0].flags.svg}
             alt=""
           />
           <div>
-            <h2>{detailCountry[0].name}</h2>
+            <h2 className="text-lg font-bold mt-6 my-4">{detailCountry[0].name}</h2>
+
+            <article className="text-sm">
+              <div> 
+              <div className="my-2 flex gap-1 items-center">
+                <p className="font-semibold">Native Name:</p>
+                <span className="font-light">{detailCountry[0].nativeName}</span>
+              </div>
+              <div className="my-2 flex gap-1">
+                <p className="font-semibold">Population:</p>
+                <span className="font-light">{detailCountry[0].population.toLocaleString()}</span>
+              </div>
+              <div className="my-2 flex gap-1">
+                <p className="font-semibold">Region:</p>
+                <span className="font-light">{detailCountry[0].region}</span>
+              </div>
+              <div className="my-2 flex gap-1">
+                <p className="font-semibold">Sub Region:</p>
+                <span className="font-light">{detailCountry[0].subregion}</span>
+              </div>
+              </div>
+            </article>
+            <div></div>
           </div>
         </article>
       </section>
